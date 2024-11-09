167e2579c7ac703c9f2d7ddc418bfff6381429b8
SAwandya <sachilaawandya@gmail.com>
2024-11-10 00:48:58 +0530

    test deployment 01

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -9,45 +9,52 @@ const showtimeRoutes = require("./routes/showTimeRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 const user = require("./routes/user");
 const auth = require("./routes/auth");
-require("./config/passport"); // Import your passport configuration
+require("./config/passport");
 const passport = require("./config/passport");
 const facebookPassport = require("./config/passport-facebook");
-require("dotenv").config(); // Load environment variables
-const session = require("express-session"); // Import express-session
-const calenderRoutes = require("./routes/calenderRoutes"); // Add this line
+require("dotenv").config();
+const session = require("express-session");
+const calenderRoutes = require("./routes/calenderRoutes");
 const path = require("path");
-const swaggerDocs = require("./swagger"); // Path to swagger setup
+const swaggerDocs = require("./swagger");
 const mailRoutes = require("./routes/mailRoutes");
 
 const mongo_url = process.env.MONGO_URL;
 
 mongoose
-  .connect(mongo_url)
-  .then(() => console.log("Connect to MongoDB"))
+  .connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Could not connect to MongoDB", err));
 
-app.use(cors()); // Enable CORS for all routes
+app.use(
+  cors({
+    origin: [
+      "https://reservenow-f7fwaxgzekbgfvgn.canadacentral-01.azurewebsites.net",
+      "http://localhost:3000",
+    ],
+  })
+);
 
-// Increase payload size for JSON and URL-encoded form data
-app.use(express.json({ limit: "10mb" })); // Set the limit (e.g., 10MB)
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use(
   session({
-    secret: "Awandya2000#", // Use a strong secret key for production
+    secret: "Awandya2000#",
     resave: true,
     saveUninitialized: true,
-    cookie: { secure: false }, // Set to true if you're using HTTPS
+    cookie: { secure: process.env.NODE_ENV === "production" },
   })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(facebookPassport.initialize());
 app.use(facebookPassport.session());
 
-// Serve Swagger docs
-swaggerDocs(app); // Initialize Swagger
+// Initialize Swagger docs
+swaggerDocs(app);
 
 app.use("/api/movies", movieRoutes);
 app.use("/api/theaters", theaterRoutes);
@@ -59,13 +66,12 @@ app.use("/api/auth", auth);
 app.use("/api", calenderRoutes);
 app.use("/api/mail", mailRoutes);
 
-// // Serve static files from the React frontend app
-// app.use(express.static(path.join(__dirname, 'dist')));
+// Serve static files from the React frontend
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-// // Catch-all route that serves the React frontend's index.html file for any route that isn't an API call
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
